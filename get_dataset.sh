cd8691d5ad38e37c727a4b7633fedb75ce97b755
Li, Sheng-Wei <pha123661@gmail.com>
2022-11-19 23:21:37 +0800

    Update download method

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,3 +1,1 @@
-gdown https://drive.google.com/u/0/uc?id=19oiRBa5Jkguvxp8nN3QOkfKBKVfKJPy-
-mkdir data
-mv train_preprocessed.pkl data/train_preprocessed.pkl
+gdown --folder https://drive.google.com/drive/folders/1P_fOXDPa4TNBGoi9XTFO7dxn1vAqwku-
