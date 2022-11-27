0a31869562fd219542ccae75ed7d7d7e928b5d3c
rgchang <roger5521610@gmail.com>
2022-11-27 13:19:08 +0800

    update latest train dataset file (x0.6 memory usage)

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,1 +1,6 @@
-gdown --folder https://drive.google.com/drive/folders/1P_fOXDPa4TNBGoi9XTFO7dxn1vAqwku-
+#!/bin/bash
+
+# train v1: 1P_fOXDPa4TNBGoi9XTFO7dxn1vAqwku-
+# train v2: 1aoevenqMc65WFmFTJA0MWK_CbVWnbOO5
+
+gdown 1aoevenqMc65WFmFTJA0MWK_CbVWnbOO5 -O train_preprocessed_v2.pkl
