0c4564c3eaf4c5b9ac969ce67e1b8aba34e96fc4
Li, Sheng-Wei <pha123661@gmail.com>
2022-11-30 16:13:03 +0800

    Update dataset version

--- get_dataset.sh
+++ get_dataset.sh
@@ -3,4 +3,4 @@
 # train v1: 1P_fOXDPa4TNBGoi9XTFO7dxn1vAqwku-
 # train v2: 1aoevenqMc65WFmFTJA0MWK_CbVWnbOO5
 
-gdown 1aoevenqMc65WFmFTJA0MWK_CbVWnbOO5 -O train_preprocessed_v2.pkl
+gdown 1KRUyStpTikVsel7RC32Pt2IV_AqAvXnt -O train_preprocessed_v4.pkl
