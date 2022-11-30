9e9b07d0d21bfe64ff59076d88e028f0b525f25a
Li, Sheng-Wei <pha123661@gmail.com>
2022-11-30 16:54:23 +0800

    Update default argument

--- Neural Network/NN_model.py
+++ Neural Network/NN_model.py
@@ -27,7 +27,7 @@ class NN_sklearn_wrapper:
         dataloader = DataLoader(dataset, self.batch_size, **kwargs)
         return dataloader
 
-    def fit(self, X, y, dev_X=None, dev_y=None, epochs=100, silent=False):
+    def fit(self, X, y, dev_X=None, dev_y=None, epochs=5, silent=False):
         train_loader = self.np_to_torchloader(
             X, y, shuffle=True, num_workers=4)
         optim = torch.optim.Adam(self.model.parameters(), lr=1e-3)
